3fc5405690cd7eca39b1fade73a9515a32d10742
David Higgins <thehig@gmail.com>
2017-06-29 17:01:24 +0100

    Add outline for further stories

---
+++ stories/Combat.stories.js
@@ -0,0 +1,35 @@
+/*
+
+## Story ideas
+
+### Combat States
+* Whose turn is it
+* Invisible/Hidden enemies
+* Who's next
+* How long has the game/turn been going
+
+
+### Combat Actions
+* Start/End Combat
+* Destroy Combat
+* Add/Remove/Set Combatant(s)
+* Next Turn
+* Automatic 'Next Round'
+
+### Combatant Actions
+* Add/Remove Note
+* Set HP
+* Set Visibility to Players (Default to hidden)
+
+### Combatant states
+
+* HP Range (0 - max)
+* HP Bounds ( 0 <= current <= max)
+* Notes
+* In/Out of turn, (Active)
+* (Un)Focused, (Focused)
+* (Un)Expanded (Expanded)
+* Visible to Players (Hidden)
+* DM only Notes
+
+*/
